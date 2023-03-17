7aa59b693eecbef24c0221f42e9aeceee23f7c7f
Hyunzzzz <simonha9721@gmail.com>
2023-03-17 23:33:48 +0900

    1331

--- src/silver/B1331.java
+++ src/silver/B1331.java
@@ -1,52 +1,75 @@
 package silver;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class B1331 {
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    static int dx[] = new int[]{1,1,2,2,-1,-1,-2,-2};
+    static int dy[] = new int[]{2,-2,1,-1,2,-2,1,-1};
+    static int isCan[][] = new int[6][6];
+    static boolean flag;
+    public static void main(String[] args) throws IOException {
 
-        boolean[][] chess = new boolean[6][6];
-        boolean[][] chess1 = new boolean[6][6];
-        String move;
-        int a=0;
-        int b=0;
-        boolean fat = false;
-        while(true){
-            move = sc.next();
-            int i=a;
-            int j=b;
-            int count=0;
-            a = move.charAt(0)-65;
-            b = move.charAt(1)-49;
-            if(chess[a][b] == true){
-                if(i==0){
-                    chess[a][b] = false;
-                }else{
-                    if(((i-a==2||a-i==2)&&(j-b==1||b-j==1))||((i-a==1||a-i==1)&&(j-b==2||b-j==2))){
-                        chess[a][b]=false;
-                    }else{
-                        fat = true;
-                        break;
-                    }
-                }
-                count++;
-                if(count==36){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String start = br.readLine();
+
+        int x = start.charAt(0) - 'A';
+        int y = start.charAt(1) - '1';
+
+        isCan[x][y] = 1;
+
+        int tx = x;
+        int ty = y;
+
+        for(int i = 0;i<35;i++) {
+            String last = br.readLine();
+
+            int lx = last.charAt(0) - 'A';
+            int ly = last.charAt(1) - '1';
+
+            flag = false;
+
+            for(int k = 0; k<8;k++) {
+                int nx = dx[k] + tx;
+                int ny = dy[k] + ty;
+
+                if (nx < 0 || 6 <= nx || ny < 0 || 6 <= ny) continue;
+
+                if (nx == lx && ny == ly && isCan[nx][ny] == 0) {
+                    isCan[lx][ly] = 1;
+                    flag = true;
                     break;
                 }
+
+
             }
-            if (chess[a][b]==false){
-                System.out.println("dsd");
+
+            if (flag) {
+                tx = lx;
+                ty = ly;
+            } else {
+                System.out.println("Invalid");
+                return;
             }
 
         }
-        if(fat==true){
-            System.out.println("Invalid");
-        }else{
+
+        flag = false;
+        for(int i = 0; i<8;i++) {
+
+            if ((tx+dx[i]) == x && (ty+dy[i]) == y ) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag) {
             System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
         }
     }
-
-
 }
+
